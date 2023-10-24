4d04de58dad60964a81509124901ab02bc35d94f
Oz Klingel <ozkl@amdocs.com>
2023-10-24 15:05:42 +0300

    deploy aws01

--- app.py
+++ app.py
@@ -1,17 +1,13 @@
-from flask import Flask
+from flask import Flask, app
 from flask_sqlalchemy import SQLAlchemy
-
 from config import SQLALCHEMY_DATABASE_URI, SECRET_KEY
-
 app = Flask(__name__)
-
 app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = SECRET_KEY
-
+db = SQLAlchemy()
+db.init_app(app)
 # register blueprints
-
-db = SQLAlchemy(app)
 from src.routes.messegaes_routes import messegaes_form_blueprint
 from src.routes.reports_routes import reports_form_blueprint
 from src.routes.onboarding_form_routes import onboarding_form_blueprint
@@ -22,10 +18,10 @@ from src.routes.userProfile_routes import userProfile_form_blueprint
 app.register_blueprint(userProfile_form_blueprint)
 app.register_blueprint(setEntityDetails_form_blueprint)
 app.register_blueprint(notification_form_blueprint)
-
 app.register_blueprint(onboarding_form_blueprint)
 app.register_blueprint(reports_form_blueprint)
 app.register_blueprint(messegaes_form_blueprint)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+
+    app.run()

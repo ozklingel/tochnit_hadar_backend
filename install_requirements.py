a9f4233887032aea804eab5ebd715958e07e24d5
Itsik Shapira <itsik.shap@gmail.com>
2024-06-02 17:22:21 +0300

    Added install_requirements.py - for installing all packages - with skipping the one's we failed on

---
+++ install_requirements.py
@@ -0,0 +1,23 @@
+import subprocess
+
+# Read the requirements.txt file
+with open('requirments.txt', 'r', encoding='utf-16') as file:
+    packages = file.readlines()
+
+list_problems = []
+list_success = []
+# Attempt to install each package individually
+for package in packages:
+    package = package.strip()
+    try:
+        subprocess.run(['pip', 'install', package], check=True)
+        list_success.append(package)
+        print(f"Successfully installed {package}\n")
+    except subprocess.CalledProcessError:
+        print(f"Failed to install {package}, skipping.\n")
+        list_problems.append(package)
+
+print("Successfully installed packages:")
+print(list_success)
+print("Failed to install packages:")
+print(list_problems)

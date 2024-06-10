03d7163a2637d275710c256d98c31902bfca3cff
Oz Klingel <ozkl@amdocs.com>
2024-06-10 19:27:52 +0300

    onboarding 01

--- db_create.sql
+++ db_create.sql
@@ -1,22 +1,64 @@
 
 DROP TABLE IF EXISTS institutions CASCADE ;
 DROP TABLE IF EXISTS clusters CASCADE ;
-DROP TABLE IF EXISTS user1 CASCADE ;
-DROP TABLE IF EXISTS apprentice CASCADE ;
-DROP TABLE IF EXISTS notifications CASCADE ;
-DROP TABLE IF EXISTS contact_forms CASCADE ;
-DROP TABLE IF EXISTS visits CASCADE ;
-DROP TABLE IF EXISTS ent_group CASCADE ;
+DROP TABLE IF EXISTS users CASCADE ;
+DROP TABLE IF EXISTS apprentices CASCADE ;
+DROP TABLE IF EXISTS tasks CASCADE ;
+DROP TABLE IF EXISTS messages CASCADE ;
+DROP TABLE IF EXISTS reports CASCADE ;
+DROP TABLE IF EXISTS regions CASCADE ;
 DROP TABLE IF EXISTS task_user_made CASCADE ;
+DROP TABLE IF EXISTS cities CASCADE ;
+DROP TABLE IF EXISTS user_role CASCADE ;
+DROP TABLE IF EXISTS role CASCADE ;
+DROP TABLE IF EXISTS base CASCADE ;
+DROP TABLE IF EXISTS work_status CASCADE ;
+DROP TABLE IF EXISTS army_role CASCADE ;
+DROP TABLE IF EXISTS spirit_status CASCADE ;
+DROP TABLE IF EXISTS serve_type CASCADE ;
+DROP TABLE IF EXISTS marriage_status CASCADE ;
+DROP TABLE IF EXISTS madadim_setting CASCADE ;
+DROP TABLE IF EXISTS message_type CASCADE ;
+DROP TABLE IF EXISTS system_report CASCADE ;
+DROP TABLE IF EXISTS gift CASCADE ;
+DROP TABLE IF EXISTS report_type CASCADE ;
+DROP TABLE IF EXISTS contacts CASCADE ;
 
+DROP TYPE IF EXISTS message_type CASCADE ;
+DROP TYPE IF EXISTS marriage_status CASCADE ;
+DROP TYPE IF EXISTS serve_type CASCADE ;
+DROP TYPE IF EXISTS spirit_status CASCADE ;
+DROP TYPE IF EXISTS army_role CASCADE ;
+DROP TYPE IF EXISTS work_status CASCADE ;
+DROP TYPE IF EXISTS report_type CASCADE ;
+DROP TYPE IF EXISTS relation_enum CASCADE ;
+
+
+CREATE TYPE message_type AS ENUM ('un known', 'mother');
+CREATE TYPE marriage_status AS ENUM ('un known', 'mother');
+CREATE TYPE serve_type AS ENUM ('un known', 'mother');
+CREATE TYPE spirit_status AS ENUM ('un known', 'mother');
+CREATE TYPE army_role AS ENUM ('un known', 'mother');
+CREATE TYPE work_status AS ENUM ('un known', 'mother');
+CREATE TYPE relation_enum AS ENUM ('un known', 'mother');
+CREATE TYPE report_type AS ENUM ('שיחה טלפונית', 'פגישה מקוונת', 'נסיון כושל', '5 הודעות', 'פגישה פיזית', 'מפגש קבוצתי', 'ביקור בבסיס', 'שיחת הורים',
+ 'ישיבת מצבר', 'הזנת מחזור', 'מפגש מחזור', 'שבת מחזור', 'עשיה לטובת בוגרים', 'כנס מלווים מקצועי חודשי', 'כנס מלווים שנתי', 'ישיבת מלוים ורכזים', 'ישיבת חודשית עם רכז', 'ישיבה מוסדית');
+
+CREATE TABLE  clusters(
+
+id uuid,
+name text DEFAULT '',
+PRIMARY KEY(id)
+
+);
 
 CREATE TABLE institutions(
-eshcol_id text DEFAULT '',
+id uuid,
+cluster_id uuid,
 roshYeshiva_phone text DEFAULT '',
 roshYeshiva_name text DEFAULT '',
 admin_phone text DEFAULT '',
 admin_name text DEFAULT '',
-id int,
 address  text DEFAULT '',
 city_id text DEFAULT '',
 phone  text DEFAULT '',
@@ -26,54 +68,62 @@ logo_path text DEFAULT '',
 name text DEFAULT '',
 owner_id text DEFAULT '',
 
-    PRIMARY KEY(id)
+PRIMARY KEY(id),
+CONSTRAINT fk_1
+  FOREIGN KEY(cluster_id)
+  REFERENCES clusters(id)
 
 );
 
-CREATE TABLE  clusters(
+
+
+CREATE TABLE  regions(
 
 id int,
 name text DEFAULT '',
-    PRIMARY KEY(id)
+PRIMARY KEY(id)
 
 );
 
  CREATE TABLE  cities(
 id int,
 name text DEFAULT '',
-cluster_id int DEFAULT 0,
-    PRIMARY KEY(id),
+region_id int ,
+PRIMARY KEY(id),
 CONSTRAINT fk_1
-      FOREIGN KEY(cluster_id)
-      REFERENCES clusters(id)
+      FOREIGN KEY(region_id)
+      REFERENCES regions(id)
 );
 
+CREATE TABLE  role(
+id uuid,
+name text DEFAULT '',
+PRIMARY KEY(id)
 
-CREATE TABLE user1(
-id int,
-role_ids text DEFAULT '',
+);
+CREATE TABLE users(
+id uuid,
 first_name text DEFAULT '',
+phone text DEFAULT '',
+
 last_name text DEFAULT '',
-teudatZehut  text DEFAULT '',
+identity_id  text DEFAULT '',
 email  text DEFAULT '',
 birthday  date ,
-eshcol  text DEFAULT '',
-institution_id  int DEFAULT 0,
+institution_id  uuid ,
 address  text DEFAULT '',
-creationDate  date ,
 photo_path  text DEFAULT 'https://www.gravatar.com/avatar',
-city_id int DEFAULT 0,
-cluster_id int DEFAULT 0,
+city_id int ,
+cluster_id uuid ,
 notifyStartWeek boolean DEFAULT False,
 notifyMorning boolean DEFAULT True,
 notifyDayBefore boolean DEFAULT False,
-
 notifyMorning_weekly_report boolean DEFAULT False,
 notifyMorning_sevev boolean DEFAULT False,
 notifyDayBefore_sevev boolean DEFAULT False,
 notifyStartWeek_sevev boolean DEFAULT False,
-
 association_date  date ,
+
 PRIMARY KEY(id)
 ,
 CONSTRAINT fk_1
@@ -88,69 +138,91 @@ CONSTRAINT fk_3
 
 );
 
-CREATE TABLE apprentice(
-id int,
-teudatZehut  text DEFAULT '',
-accompany_id int ,
+CREATE TABLE  user_role(
+user_id uuid,
+role_id uuid,
+PRIMARY KEY(user_id,role_id),
+CONSTRAINT fk_1
+      FOREIGN KEY(user_id)
+      REFERENCES users(id),
+CONSTRAINT fk_2
+      FOREIGN KEY(role_id)
+      REFERENCES role(id)
+
+);
+
+
+
+
+CREATE TABLE base (
+id uuid,
+ name TEXT DEFAULT ''::text,
+ cordinatot TEXT DEFAULT ''::text,
+  PRIMARY KEY (id));
+
+CREATE TABLE  madadim_setting(
+cenes_madad_date date DEFAULT '1999-09-09',
+tochnitMeet_madad_date date DEFAULT '1999-09-09',
+eshcolMosadMeet_madad_date date DEFAULT '1999-09-09',
+mosadYeshiva_madad_date date DEFAULT '1999-09-09',
+hazana_madad_date date DEFAULT '1999-09-09',
+professionalMeet_madad_date date DEFAULT '1999-09-09',
+matzbarmeet_madad_date date DEFAULT '1999-09-09',
+doForBogrim_madad_date date DEFAULT '1999-09-09',
+basis_madad_date date DEFAULT '1999-09-09',
+callHorim_madad_date date DEFAULT '1999-09-09',
+groupMeet_madad_date date DEFAULT '1999-09-09',
+meet_madad_date date DEFAULT '1999-09-09',
+call_madad_date date DEFAULT '1999-09-09'
+);
+
+
+CREATE TABLE  contacts(
+id uuid,
+phone text DEFAULT '',
+name text DEFAULT '',
+email text DEFAULT '',
+relation relation_enum,
+PRIMARY KEY(id)
+
+);
+
+CREATE TABLE apprentices(
+id uuid,
+identity_id  text DEFAULT '',
+accompany_id uuid ,
 last_name text DEFAULT '',
-maritalStatus text DEFAULT '',
+marriage_status_id marriage_status,
 marriage_date date ,
-marriage_date_ivry text DEFAULT 'ה' בטבת',
-teacher_grade_b_phone text DEFAULT '',
-teacher_grade_b text DEFAULT '',
-teacher_grade_b_email  text DEFAULT '',
-teacher_grade_a_phone text DEFAULT '',
-teacher_grade_a_email  text DEFAULT '',
-teacher_grade_a text DEFAULT '',
-highSchoolInstitution text DEFAULT '',
-high_school_teacher_phone text DEFAULT '',
-high_school_teacher text DEFAULT '',
-high_school_name text DEFAULT '',
-high_school_teacher_email text DEFAULT '',
-contact1_email text DEFAULT '',
-contact1_first_name text DEFAULT '',
-contact1_last_name text DEFAULT '',
-contact1_phone text DEFAULT '',
-contact1_relation text DEFAULT '',
-contact2_email text DEFAULT '',
-contact2_phone text DEFAULT '',
-contact2_first_name text DEFAULT '',
-contact2_last_name text DEFAULT '',
-contact2_relation text DEFAULT '',
-contact3_phone text DEFAULT '',
-contact3_first_name text DEFAULT '',
-contact3_last_name text DEFAULT '',
-contact3_email text DEFAULT '',
-contact3_relation text DEFAULT '',
-militaryCompoundId  int DEFAULT 14509,
+marriage_date_ivry text DEFAULT '',
+military_compound_id  uuid ,
 unit_name text DEFAULT '',
-serve_type text DEFAULT '',
+serve_type serve_type,
 paying boolean DEFAULT False,
 release_date date,
 recruitment_date date ,
 onlineStatus int DEFAULT 0,
-matsber text DEFAULT '',
+matsber spirit_status,
 thPeriod text DEFAULT '',
-password text DEFAULT '',
 phone  text DEFAULT '',
 email  text DEFAULT '',
 birthday  date ,
-eshcol  text DEFAULT '',
-institution_id  int DEFAULT 0,
+cluster_id uuid,
+institution_id  uuid ,
 address  text DEFAULT '',
 creationDate  text DEFAULT '',
 photo_path  text DEFAULT 'https://www.gravatar.com/avatar',
-city_id int DEFAULT 0,
-cluster_id int DEFAULT 0,
-army_role text DEFAULT '',
+city_id int ,
+army_role army_role,
 militaryPositionOld text DEFAULT ''   ,
 militaryUpdatedDateTime date ,
+highschoolinstitution text DEFAULT '',
 educationalInstitution text DEFAULT '',
 educationFaculty text DEFAULT '',
 workOccupation text DEFAULT '',
 workType text DEFAULT '',
 workPlace text DEFAULT '',
-workStatus text DEFAULT '',
+work_status work_status,
 militaryPositionNew text DEFAULT '',
 first_name text DEFAULT '',
 institution_mahzor text DEFAULT '',
@@ -166,18 +238,23 @@ CONSTRAINT fk_2
       FOREIGN KEY(city_id)
       REFERENCES cities(id),
 CONSTRAINT fk_4
-      FOREIGN KEY(militarycompoundid )
+      FOREIGN KEY(military_compound_id )
       REFERENCES Base(id),
+CONSTRAINT fk_5
+      FOREIGN KEY(accompany_id)
+      REFERENCES users(id),
 CONSTRAINT fk_3
       FOREIGN KEY(cluster_id)
       REFERENCES clusters(id)
 
+
 );
 
-CREATE TABLE  contact_forms(
-id int,
-created_by_id int  ,
-created_for_id int  ,
+
+CREATE TABLE  messages(
+id uuid,
+created_by_id uuid  ,
+created_for_id uuid  ,
 created_at timestamp ,
 subject text DEFAULT '',
 content text DEFAULT '',
@@ -185,28 +262,32 @@ allreadyread boolean DEFAULT False,
 attachments text[] default '{}',
 icon text DEFAULT 'empty',
 description text DEFAULT '',
-type text DEFAULT 'draft',
+type message_type,
 ent_group  text DEFAULT '',
 PRIMARY KEY(id,created_for_id),
 CONSTRAINT fk_1
       FOREIGN KEY(created_by_id)
-      REFERENCES user1(id),
+      REFERENCES users(id),
+
 CONSTRAINT fk_2
       FOREIGN KEY(created_for_id)
-      REFERENCES user1(id)
+      REFERENCES users(id)
 
 
 );
 
-CREATE TABLE  visits(
 
-id int,
-ent_reported int  ,
-user_id int  ,
+
+CREATE TABLE  reports(
+
+id uuid,
+apprentice_reported  uuid  ,
+user_reported  uuid  ,
+user_id uuid  ,
 visit_in_army boolean DEFAULT False,
-visit_date DATE ,
+reports_date timestamp ,
 note text DEFAULT '',
-title text DEFAULT '',
+title report_type,
 attachments text[] default '{}',
 description text DEFAULT '',
 allreadyread boolean DEFAULT False,
@@ -214,45 +295,21 @@ ent_group  text DEFAULT ''  ,
 created_at    timestamp,
 CONSTRAINT fk_1
       FOREIGN KEY(user_id)
-      REFERENCES user1(id)
-
-
-);
-CREATE TABLE  notifications(
-
-id int,
-subject text DEFAULT '' ,
-userid int ,
-event text DEFAULT '',
-date DATE ,
-created_at timestamp,
-allreadyread boolean DEFAULT False,
-numOfLinesDisplay int  ,
-details text DEFAULT '',
-frequency text DEFAULT '',
-institution_id boolean DEFAULT False,
-PRIMARY KEY(id),
-CONSTRAINT fk_1
-      FOREIGN KEY(userid)
-      REFERENCES user1(id)
-
+      REFERENCES users(id),
+CONSTRAINT fk_2
+      FOREIGN KEY(user_reported)
+      REFERENCES users(id),
+CONSTRAINT fk_3
+      FOREIGN KEY(apprentice_reported)
+      REFERENCES apprentices(id)
 
 
 );
 
 
 
-CREATE TABLE  ent_group(
 
-id int,
-user_id int,
-group_name text DEFAULT '',
-PRIMARY KEY(id),
-CONSTRAINT fk_1
-      FOREIGN KEY(user_id)
-      REFERENCES user1(id)
 
-);
 
 CREATE TABLE  gift(
 
@@ -264,27 +321,26 @@ PRIMARY KEY(code)
 
 CREATE TABLE  system_report(
 
-id int,
+id uuid,
 creation_date DATE DEFAULT '1999-09-09' ,
 type text DEFAULT '',
-related_id int,
-value int,
+related_id uuid,
+value text DEFAULT '',
 PRIMARY KEY(id)
 
 );
-CREATE TABLE base (
-id INTEGER NOT NULL,
- name TEXT DEFAULT ''::text,
- cordinatot TEXT DEFAULT ''::text,
-  PRIMARY KEY (id));
 
 
-CREATE TABLE  task_user_made(
+CREATE TABLE  tasks(
 
-id int,
-userid int ,
+id uuid,
+userid uuid ,
+apprentice_reported  uuid  ,
+user_reported  uuid  ,
+institution_reported uuid,
 event text DEFAULT '',
 date timestamp ,
+allreadyread boolean DEFAULT False,
 created_at timestamp,
 Frequency_end text DEFAULT '' ,
 Frequency_weekday text DEFAULT '',
@@ -292,13 +348,59 @@ Frequency_meta text DEFAULT ''  ,
 details text DEFAULT '',
 status text DEFAULT '',
 
-institution_id boolean DEFAULT False,
 PRIMARY KEY(id),
 CONSTRAINT fk_1
       FOREIGN KEY(userid)
-      REFERENCES user1(id)
-
-
+      REFERENCES users(id)
+,
+CONSTRAINT fk_2
+      FOREIGN KEY(user_reported)
+      REFERENCES users(id),
+CONSTRAINT fk_3
+      FOREIGN KEY(apprentice_reported)
+      REFERENCES apprentices(id)
+,CONSTRAINT fk_4
+      FOREIGN KEY(institution_reported)
+      REFERENCES institutions(id)
 
 );
+
+
+
+
+INSERT INTO regions (id, name) VALUES (0,'ירושלים והסביבה');;
+INSERT INTO regions (id, name) VALUES (1,'יהודה ושומרון');
+INSERT INTO regions (id, name) VALUES (2,'מחוז דרום');
+INSERT INTO regions (id, name) VALUES (3,'מחוז צפון');
+INSERT INTO regions (id, name) VALUES (4,'תל אביב והמרכז');
+INSERT INTO regions (id, name) VALUES (5,'un known');
+
+INSERT INTO clusters (id, name) VALUES ('bd65600d-8669-4903-8a14-af88203ad130', 'un known');
+
+INSERT INTO base (id, name,cordinatot) VALUES ('bd65600d-8669-4903-8a14-af88203ad130','un known', '31.26097751244561 34.81765168465695');
+INSERT INTO cities (id, name,region_id) VALUES (0,'un known', 0);
+INSERT INTO institutions (id, name,cluster_id,owner_id) VALUES ('bd65600d-8669-4903-8a14-af88203ad130', 'un known','bd65600d-8669-4903-8a14-af88203ad130','bd65600d-8669-4903-8a14-af88203ad131');
+
+
+
+INSERT INTO role (id, name) VALUES ('bd65600d-8669-4903-8a14-af88203ad130','מלווה');
+INSERT INTO role (id, name) VALUES ('bd65600d-8669-4903-8a14-af88203ad131','רכז מוסד');
+INSERT INTO role (id, name) VALUES ('bd65600d-8669-4903-8a14-af88203ad132','רכז אשכול');
+INSERT INTO role (id, name) VALUES ('bd65600d-8669-4903-8a14-af88203ad133','אחראי תוכנית');
+
+INSERT INTO madadim_setting (cenes_madad_date, tochnitmeet_madad_date, eshcolmosadmeet_madad_date, mosadyeshiva_madad_date, hazana_madad_date, professionalmeet_madad_date, matzbarmeet_madad_date, doforbogrim_madad_date, basis_madad_date, callhorim_madad_date, groupmeet_madad_date, meet_madad_date, call_madad_date) VALUES
+ ('1999-02-02', '1999-02-02', '1999-02-02', '1999-02-02', '1999-02-02', '1999-02-02', '1999-02-02', '1999-02-02', '1999-02-02', '1999-02-02', '1999-02-02', '1999-02-02', '1999-02-02');
+
+INSERT INTO users (id, first_name, phone, last_name, identity_id, email, birthday, institution_id, address, photo_path, city_id, cluster_id, notifystartweek, notifymorning, notifydaybefore, notifymorning_weekly_report, notifymorning_sevev, notifydaybefore_sevev, notifystartweek_sevev, association_date) VALUES
+ ('bd65600d-8669-4903-8a14-af88203ad130', 'as', '549247616', 'saasd', 'asdasd', 'zxc', '1999-03-03', 'bd65600d-8669-4903-8a14-af88203ad130', '', '', 0, 'bd65600d-8669-4903-8a14-af88203ad130',True , True, True, True, True, True, True, '1999-02-02');
+INSERT INTO users (id, first_name, phone, last_name, identity_id, email, birthday, institution_id, address, photo_path, city_id, cluster_id, notifystartweek, notifymorning, notifydaybefore, notifymorning_weekly_report, notifymorning_sevev, notifydaybefore_sevev, notifystartweek_sevev, association_date) VALUES
+ ('bd65600d-8669-4903-8a14-af88203ad131', 'as', 'xcasddsa', 'saasd', 'asdasd', 'zxc', '1999-03-03', 'bd65600d-8669-4903-8a14-af88203ad130', '', '', 0, 'bd65600d-8669-4903-8a14-af88203ad130',True , True, True, True, True, True, True, '1999-02-02');
+INSERT INTO users (id, first_name, phone, last_name, identity_id, email, birthday, institution_id, address, photo_path, city_id, cluster_id, notifystartweek, notifymorning, notifydaybefore, notifymorning_weekly_report, notifymorning_sevev, notifydaybefore_sevev, notifystartweek_sevev, association_date) VALUES
+ ('bd65600d-8669-4903-8a14-af88203ad132', 'as', 'xcasddsa', 'saasd', 'asdasd', 'zxc', '1999-03-03', 'bd65600d-8669-4903-8a14-af88203ad130', '', '', 0, 'bd65600d-8669-4903-8a14-af88203ad130',True , True, True, True, True, True, True, '1999-02-02');
+
+INSERT INTO user_role (user_id, role_id) VALUES ('bd65600d-8669-4903-8a14-af88203ad130', 'bd65600d-8669-4903-8a14-af88203ad130');
+INSERT INTO user_role (user_id, role_id) VALUES ('bd65600d-8669-4903-8a14-af88203ad131', 'bd65600d-8669-4903-8a14-af88203ad131');
+
+INSERT INTO apprentices (id, identity_id,work_status,city_id,army_role,institution_id,cluster_id,matsber,serve_type,military_compound_id, accompany_id,  marriage_status_id,last_name, marriage_date, marriage_date_ivry, unit_name,  paying, release_date, recruitment_date, onlinestatus,  thperiod, phone, email, birthday,  address, creationdate, photo_path,   militarypositionold, militaryupdateddatetime, highschoolinstitution, educationalinstitution, educationfaculty, workoccupation, worktype, workplace,  militarypositionnew, first_name, institution_mahzor, association_date, birthday_ivry) VALUES
+('bd65600d-8669-4903-8a14-af88203ad130', 'bd65600d-8669-4903-8a14-af88203ad130', 'un known', 0, 'un known', 'bd65600d-8669-4903-8a14-af88203ad130', 'bd65600d-8669-4903-8a14-af88203ad130', 'un known', 'un known', 'bd65600d-8669-4903-8a14-af88203ad130', 'bd65600d-8669-4903-8a14-af88203ad130', 'un known', '', '1999-09-09', '', '', True, '1999-09-09', '1999-09-09', 0, '', '', '', '1999-09-09', 0, '', '', '', '1999-09-09', '', '', '', '', '', '', '', '', '', '1999-09-09', '');
 

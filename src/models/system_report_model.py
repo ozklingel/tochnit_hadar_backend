7174a19c2e27e08db28232a2c4a683e524efd3b8
Yam Horesh <yam.horesh@pm.me>
2024-06-17 12:16:09 +0300

    feat: update madadim setting and system report to new format

--- src/models/system_report_model.py
+++ src/models/system_report_model.py
@@ -1,11 +1,12 @@
 from src.models.models_defines import ID_COL
 from src.services import db
+from sqlalchemy.dialects.postgresql import UUID
 
 
 class SystemReport(db.Model):
-    __tablename__ = "system_report_old"
+    __tablename__ = "system_report"
 
-    id = db.Column(ID_COL, db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = db.Column(ID_COL, UUID(as_uuid=True), primary_key=True, autoincrement=True, nullable=False)
     creation_date = db.Column("creation_date", db.DateTime, nullable=False)
     type = db.Column("type", db.String(20), nullable=False)
     related_id = db.Column("related_id", db.Integer, nullable=False)

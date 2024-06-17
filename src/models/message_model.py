012c57ba4235ad66a44f49efa7df164c083c2703
Yam Horesh <yam.horesh@pm.me>
2024-06-17 13:16:37 +0300

    fix: set gift and message ids as uuid

--- src/models/message_model.py
+++ src/models/message_model.py
@@ -1,8 +1,9 @@
 from datetime import datetime
 from sqlalchemy import ARRAY, ForeignKey
+from sqlalchemy.dialects.postgresql import UUID
 
 from src.services import db
-from src.models.models_defines import WAS_READ_COL
+from src.models.models_defines import ID_COL, WAS_READ_COL
 
 from .user_model import User
 
@@ -11,12 +12,12 @@ from .user_model import User
 class Message(db.Model):
     __tablename__ = "messages"
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(ID_COL,UUID(as_uuid=True), primary_key=True)
     subject = db.Column(db.String(50), nullable=False, default="")
     content = db.Column(db.String(250), nullable=False, default="")
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    created_by_id = db.Column(db.Integer, ForeignKey(User.id), nullable=False)
-    created_for_id = db.Column(db.Integer, ForeignKey(User.id), nullable=False)
+    created_by_id = db.Column(UUID(as_uuid=True), ForeignKey(User.id), nullable=False)
+    created_for_id = db.Column(UUID(as_uuid=True), ForeignKey(User.id), nullable=False)
     allreadyread = db.Column(WAS_READ_COL, db.Boolean, nullable=True)
     attachments = db.Column(ARRAY(db.String), nullable=False)
     icon = db.Column(db.String(20), nullable=False, default="")
